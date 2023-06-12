1b74fe1d128eef9bba58018bee995174bc61dc18
Spellthorn <thespellthorn@gmail.com>
2023-06-11 21:03:07 -0400

    Added Fullscreen/Landscape for mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 // Entry point of the application
 void main() {
+  // Ensure Flutter is initialized before running the app
+  WidgetsFlutterBinding.ensureInitialized();
+  // Changes mobile to fullscreen
+  Flame.device.fullScreen();
+  // Changes mobile layout to Landscape
+  Flame.device.setLandscape();
+
   // Create an instance of the Pixel Adventure game
   PixelAdventure game = PixelAdventure();
 
