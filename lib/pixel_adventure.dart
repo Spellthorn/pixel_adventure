12453899cbd8afa097a8e05f4bd83c9bf3410e83
Spellthorn <thespellthorn@gmail.com>
2023-06-11 20:54:36 -0400

    Updated Starter Project

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,20 +1,3 @@
-import 'dart:async';
-
 import 'package:flame/game.dart';
-import 'package:pixel_adventure/actors/player.dart';
 
-class PixelAdventure extends FlameGame {
-  final Player _player = Player();
-  @override
-  FutureOr<void> onLoad() async {
-    // Load and cache all images for efficient access during gameplay
-    await images.loadAllImages();
-
-    // Add the player character to the game scene
-    add(_player);
-
-    // Retrieve and return the remaining onload events by invoking the parent class's onLoad() method.
-    // This ensures that any additional onload events registered by the parent class are properly processed.
-    return super.onLoad();
-  }
-}
+class PixelAdventure extends FlameGame {}
